d425ed0f2e3de3047caf6c7aefca7f26e1cbf893
Léonard Wyrsch <leonard.wyrsch@stud.hslu.ch>
2020-10-02 15:09:16 +0200

    add hello world project

---
+++ Hello World/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hello_World
+{
+    class Program
+    {
+        private static readonly Dictionary<string, Func<string>> map = new Dictionary<string, Func<string>>()
+        {
+            ["random"] = GetRandom
+        };
+
+        static void Main(string[] args)
+        {
+            string arg = args.FirstOrDefault();
+            string message = map.ContainsKey(arg) ? map[arg]() : "Hello World";
+            Console.WriteLine(message);
+        }
+
+        static string GetRandom()
+        {
+            return new Random().Next(100).ToString();
+        }
+    }
+}
