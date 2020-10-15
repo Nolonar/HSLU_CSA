492965730debf1900d686c9b8e23eb6574d753e5
Wyrsch Léonard <leonard.wyrsch@stud.hslu.ch>
2020-10-16 00:19:17 +0200

    improve GravityPong

--- Project/GravityPong/Ball.cs
+++ Project/GravityPong/Ball.cs
@@ -4,14 +4,15 @@ namespace Project.GravityPong
 {
     class Ball : Pong.Ball
     {
-        public readonly Vector2 Gravity = new Vector2(0, 10f / Unit.Second);
+        public readonly Vector2 Gravity = new Vector2(0, 0.000001f / Unit.Second);
 
         public Ball(int radius) : base(radius) { /* No additional code. */ }
 
         public override void UpdatePosition(long delta)
         {
             base.UpdatePosition(delta);
-            Direction += Gravity * delta;
+            if (IsMoving)
+                Direction += Gravity * delta;
         }
     }
 }
